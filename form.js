08d1a23fc6ee1ccff4689720d1256af32a9c67de
Vinay Singh Rawat <vinaysinghrawat505@gmail.com>
2025-05-14 10:38:22 +0530

    created a notification panel

--- form.js
+++ form.js
@@ -2,6 +2,10 @@ let form = document.querySelector("#my-form");
 let name = document.querySelector("#name");
 let email = document.querySelector("#email");
 let message = document.querySelector("#message");
+let btn = document.querySelector("#my-form-button")
+let popup = document.querySelector(".notication-popup");
+let popupText = document.querySelector(".notify-text");
+
 
 const handleForm = () => {
     function handleSubmit(event) {
@@ -12,10 +16,11 @@ const handleForm = () => {
         }
 
         (async function () {
-            const KEY = 'pYZXl3V3AD3olAwP4aVfeJrM1AVT47byg';
+            btn.innerText = "Wait.."
+            btn.disabled = true
+            const KEY = 'FJwx8RmHlVnt7c9XGrBKNYUQqwqZbBfA';
             let URL = 'https://intact-roanna-api-v9-6a640f42.koyeb.app/api/public/submit';
             try {
-                console.log("Submitting Form")
                 const response = await fetch(URL, {
                     method: 'POST',
                     headers: {
@@ -29,12 +34,21 @@ const handleForm = () => {
                         message: message.value
                     }),
                 });
-            } catch (error) {
-                alert("Server Not Responding");
-                console.log(error);
-            } finally {
-                console.log("Form Submitted");
+
+                if (response.ok) {
+                    showPopup();
                 form.reset();
+                console.log("form Submitted");
+                }else{
+                alert("I appogize Server Not Responding");
+                
+                }
+
+            } catch (error) {
+                console.log(error);
+            }finally{
+                btn.disabled = false;
+                btn.innerText = "Submit";
             }
         })();
     }
@@ -98,6 +112,14 @@ function removeError(input) {
     input.style.border = "none";
 
     error.style.visibility = "hidden";
+}
+
+function showPopup(){
+    popup.style.top = "1%";
+    let time = setTimeout(function(){
+        popup.style.top = "-100%";
+        clearTimeout(time);
+    }, 3000);
 }
 
 export default handleForm;
